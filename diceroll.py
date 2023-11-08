53fd9b7d4029b09697b84aa32897b685a54e1234
robik3456 <kakucs.robert@gmail.com>
2023-11-09 00:42:46 +0100

    Diceroll - Min value

--- diceroll.py
+++ diceroll.py
@@ -6,6 +6,7 @@ roll_again = "yes"
 
 total = 0
 total_total = 0
+min_total = 20
 max_total = 0
 rolls = 0
 
@@ -21,11 +22,15 @@ while roll_again == "yes" or roll_again == "y":
     total = value1 + value2 + value3
     total_total = total_total + total
 
+    if total < min_total:
+        min_total = total
+
     if total > max_total:
         max_total = total
 
     print(value1,value2,value3)
     print("The total value of this roll is", total)
+    print("Your lowest roll so far was", min_total)
     print("Your highest roll so far was", max_total)
     print("The total value of your rolls so far was", total_total, "by rolling", rolls, "times. This is an average of:", round(total_total / rolls,2))
     print()
